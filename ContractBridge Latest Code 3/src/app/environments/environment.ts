b78c5092a9f5b7b0bcd402422ef950bd3be70400
Priyanka Rana <priyanka.rana@evontech.com>
2022-11-23 17:33:53 +0530

    removed unwanted files

--- ContractBridge Latest Code 3/src/app/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    socketHost: 'ws://localhost',
-    socketPort: '3001',
-    host: 'http://3.130.228.54:3000/api/'
-  };
-  
--- ContractBridge Latest Code 3/src/app/playersvo.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Playersvo } from './playersvo';
-
-describe('Playersvo', () => {
-  it('should create an instance', () => {
-    expect(new Playersvo()).toBeTruthy();
-  });
-});
